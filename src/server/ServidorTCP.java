c8a04e90578a865214fe48386e5cbd92e257d0b7
marcosvcg <marcos.vdcg@gmail.com>
2026-05-24 19:44:11 -0300

    FEAT: permitir multiplas conexoes

--- src/server/ServidorTCP.java
+++ src/server/ServidorTCP.java
@@ -9,47 +9,71 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /*
- * Servidor TCP implementado seguindo o passo a passo do artigo do DevMedia
+ * Servidor TCP implementado e adaptado seguindo o artigo do DevMedia como referencia
  * 
  * leiam, o artigo esta sensacional!!
  * Link: https://www.devmedia.com.br/java-socket-entendendo-a-classe-socket-e-a-serversocket-em-detalhes/31894
  */
 
-public class ServidorTCP {
+public class ServidorTCP implements Runnable {
 
 	public static final int PORT = 33;
+	private Socket cliente;
+
+	public ServidorTCP(Socket cliente) {
+		this.cliente = cliente;
+	}
 	
 	public static void main(String[] args) {
 		try {
 			ServerSocket servidor = new ServerSocket(PORT);
 			System.out.println("Servidor iniciado na porta " + PORT);
 
-			Socket cliente = servidor.accept();
-			System.out.println("Cliente conectado do IP " + cliente.getInetAddress().getHostAddress());
-			
-			PrintStream saida = new PrintStream(cliente.getOutputStream());
+			while(true) {
+				Socket cliente = servidor.accept();
+				ServidorTCP handler = new ServidorTCP(cliente);
+				
+				Thread conexao = new Thread(handler);
+				conexao.start();
+			}
+
+			//servidor.close();
+
+		} catch (IOException e) {
+			Logger.getLogger(ServidorTCP.class.getName())
+			.log(Level.SEVERE, null, e);
+		}
+	}
+
+	@Override
+	public void run() {
+		try {
+
+			String clienteIP = cliente.getInetAddress().getHostAddress();
+			System.out.println("Cliente conectado do IP " + clienteIP);
+
 			Scanner entrada = new Scanner(cliente.getInputStream());
-			
+			PrintStream saida = new PrintStream(cliente.getOutputStream());
+
 			while(entrada.hasNextLine()) {
 				String mensagem = entrada.nextLine();
-				
+
 				if(mensagem.equals("/exit")) {
-					System.out.println("Conexão encerrada!");
+					System.out.printf("\nConexão com [%s] encerrada", clienteIP);
 					saida.println("Conexão encerrada!");
 					break;
 				}
-				
-				System.out.println(mensagem);
+
+				System.out.printf("\n[%s]: %s", clienteIP, mensagem);
 			}
-			
+
 			entrada.close();
 			saida.close();
 			cliente.close();
-			servidor.close();
 			
 		} catch (IOException e) {
 			Logger.getLogger(ServidorTCP.class.getName())
-				.log(Level.SEVERE, null, e);
+			.log(Level.SEVERE, null, e);
 		}
 	}
 }
